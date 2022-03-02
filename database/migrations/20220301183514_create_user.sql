a3231ff763ca95ab5f8cdee00e128fc2f91b40e0
Valak-Code <khanhparkour12345@gmail.com>
2022-03-02 10:10:33 +0700

    completed

--- database/migrations/20220301183514_create_user.sql
+++ database/migrations/20220301183514_create_user.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
-INSERT INTO user (id,created_at,updated_at,first_name,last_name,email,password,phone) VALUES
+-- +goose StatementEnd
+INSERT INTO users (id,created_at,updated_at,first_name,last_name,email,password,phone) VALUES
 (1,NOW(),NOW(),'Phi','Khanh','khanh@gmail.com','pass','phone')
 ;
--- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 SELECT 'down SQL query';
-DELETE FROM user where id = 1;
 -- +goose StatementEnd
+DELETE FROM users where id = 1;
 
--- database/migrations/20220301200458_create_user.go
+++ database/migrations/20220301200458_create_user.go
@@ -12,22 +12,18 @@ func init() {
 
 func upCreateUser(tx *sql.Tx) error {
 	// This code is executed when the migration is applied.
-	_, err := tx.Exec("INSERT INTO user (id,created_at,updated_at,first_name,last_name,email,password,phone) VALUES (2,NOW(),NOW(),'Phi','Khanh2','khanh2@gmail.com','pass','phone')")
+	_, err := tx.Exec("INSERT INTO users (id,created_at,updated_at,first_name,last_name,email,password,phone) VALUES (2,NOW(),NOW(),'Phi','Khanh2','khanh2@gmail.com','pass','phone')")
 	if err != nil {
-		tx.Rollback()
 		return err
 	}
-	tx.Commit()
 	return nil
 }
 
 func downCreateUser(tx *sql.Tx) error {
 	// This code is executed when the migration is rolled back.
-	_, err := tx.Exec("DELETE FROM user where id = 2")
+	_, err := tx.Exec("DELETE FROM users where id = 2")
 	if err != nil {
-		tx.Rollback()
 		return err
 	}
-	tx.Commit()
 	return nil
 }
